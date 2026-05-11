16b14d7bb26e995a608c77b4d633f807ea3e19fc
maheerali121@gmail.com <maheerali121@gmail.com>
2026-05-11 21:04:06 +0500

    excessive events fixed

--- src/game.ts
+++ src/game.ts
@@ -120,15 +120,16 @@ export class Game {
         .filter((state) => stateOwnerChanges.includes(state.id))
         .map((state) => ({ id: state.id, ownerId: state.ownerId })),
     });
+
+    this.batchMovements = this.batchMovements.filter(
+      (batchMovement) => batchMovement.unitsCollided <= batchMovement.amount,
+    );
     if (unitChanges.length > 0) {
       sendEventToRoom(this.id, {
         type: "update-unit-counts",
         data: unitChanges,
       });
     }
-    this.batchMovements = this.batchMovements.filter(
-      (batchMovement) => batchMovement.unitsCollided <= batchMovement.amount,
-    );
   }
 
   private createBatchMovement(fromStateId: string, toStateId: string, amount: number) {
